5c2a767947cfd9d2ed6befa7a51e0b2bf09b3b11
StanislavSemashko <stanislavsemashko5@gmail.com>
2018-09-20 11:33:23 +0200

    works

--- hello_world.c
+++ hello_world.c
@@ -1,3 +1,15 @@
+/*----------------------------------------------------------
+ *				HTBLA-Leonding / Class: <2Dhif
+ * ---------------------------------------------------------
+ * Exercise Number: 1
+ * Title:			Hello World
+ * Author:			Stanislav Siamashka
+ * ----------------------------------------------------------
+ * Description:
+ * Prints Hello World
+ * ----------------------------------------------------------
+ */
+
 #include "stdio.h"
 
 int main(int argc, char const *argv[]) {
